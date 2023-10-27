9e9835ea38d3bd9922c298e4ccd7f20f31c4c8d0
ian_hg <ian.hughes.gunther@gmail.com>
2023-10-27 13:02:18 -0700

    changed side-nav link from Billing to Plotly

--- run.py
+++ run.py
@@ -11,6 +11,7 @@ from   sys import exit
 from apps.config import config_dict
 from apps import create_app, db
 
+
 # WARNING: Don't run with debug turned on in production!
 DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 
