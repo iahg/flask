c3a5df7996b84ee02448f5751d4eac46dcc1b4d6
ian_hg <ian.hughes.gunther@gmail.com>
2023-10-27 11:08:09 -0700

    updated tables and streamlit
    white background
    improved visuals on streamlit
    expand streamlit within container

--- apps/streamlit/graph.py
+++ apps/streamlit/graph.py
@@ -2,6 +2,19 @@ import streamlit as st
 import plotly.graph_objects as go
 import numpy as np
 
+st.markdown("""
+    <style>
+        .reportview-container {
+            margin-top: -2em;
+        }
+        #MainMenu {visibility: hidden;}
+        .stDeployButton {display:none;}
+        footer {visibility: hidden;}
+        #stDecoration {display:none;}
+    </style>
+""", unsafe_allow_html=True)
+
+
 # Create a grid of points
 x = np.linspace(-5, 5, 100)
 y = np.linspace(-5, 5, 100)
@@ -30,8 +43,9 @@ fig.update_layout(
         yaxis_title='Y Axis Label',
         zaxis_title='Z Axis Label',
     ),
-    title='3D Surface Plot of a Complex Function with White Background and Black Text/Axis'
+    title='3D surface'
 )
 
 # Render the Plotly figure using st.plotly_chart
-st.plotly_chart(fig)
+st.plotly_chart(fig, use_container_height=True)
+
